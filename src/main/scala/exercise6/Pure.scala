303de8f03c8a3ee43d118c1d693d90bc49fa3fbb
Karthik Rajagopal <eternal.hacker@gmail.com>
2018-03-04 18:46:58 -0800

    some more state machine

--- src/main/scala/exercise6/Pure.scala
+++ src/main/scala/exercise6/Pure.scala
@@ -146,6 +146,26 @@ object RNG {
   object State {
     def unit[S, A](a: A): State[S, A] = State(s => (a, s))
 
+    def modify[S](f: S => S): State[S, Unit] = for {
+      s <- get
+      _ <- set(f(s))
+    } yield ()
+
+    def get[S]: State[S, S] = State(s => (s, s))
+
+    def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+  }
+
+  sealed trait Input
+  case object Coin extends Input
+  case object Turn extends Input
+
+  case class Machine(locked: Boolean, candies: Int, coins: Int) {
+    def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+  }
+
+  object Machine {
+    def apply(locked: Boolean, candies: Int, coins: Int): Machine = Machine(locked, candies, coins)
   }
 
 }
