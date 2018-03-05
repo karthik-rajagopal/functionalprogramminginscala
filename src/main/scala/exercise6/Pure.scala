05ddb6b5d9d25f0de153c8fabd4d10a9a8f4e859
Karthik Rajagopal <eternal.hacker@gmail.com>
2018-03-04 18:32:58 -0800

    state machine

--- src/main/scala/exercise6/Pure.scala
+++ src/main/scala/exercise6/Pure.scala
@@ -122,6 +122,32 @@ object RNG {
     }
   }
 
+  case class State[S, +A](run: S => (A, S)) {
+
+    def map[B](f: A => B): State[S, B] = {
+      State(s => {
+        val (a, newS) = run(s)
+        (f(a), newS)
+      })
+    }
+
+    def map2[B, C](s: State[S, B])(f: (A, B) => C): State[S, C] = {
+      flatMap(a => s.map(b => f(a, b)))
+    }
+
+    def flatMap[B](f: A => State[S, B]): State[S, B] = {
+      State(s => {
+        val (a, newS) = run(s)
+        f(a).run(newS)
+      })
+    }
+  }
+
+  object State {
+    def unit[S, A](a: A): State[S, A] = State(s => (a, s))
+
+  }
+
 }
 
 
